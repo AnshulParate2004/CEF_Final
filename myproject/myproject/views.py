e32efaaf500c472830adc8b8053285e06e08cf23
AnshulParate2004 <anshulnparate@gmail.com>
2025-04-11 23:17:05 +0530

    chnages

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-def signin_view(request):
+def signin(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            if user.is_staff:
+                return redirect('/admin/')
+            else:
+                return redirect('/welcome/')
+        else:
+            messages.error(request, "Invalid username or password")
+
     return render(request, 'signin.html')
+
 
 def about(request):
     return render(request, 'about.html')
@@ -13,8 +31,6 @@ def help(request):
 def contact(request):
     return render(request, 'contact.html')
 
-def signin(request):
-    return render(request, 'signin.html')
 
 def index(request):
     return render(request, 'index.html')
